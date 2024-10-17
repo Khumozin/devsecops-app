acd75620817ccb28be008261c6d9a37f9663c9b7
Khumo Mogorosi <khumomogorosi@gmail.com>
2024-10-17 18:28:49 +0200

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -24,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, DevSecOpsApp');
+    expect(compiled.querySelector('span')?.textContent).toContain('DevSecOps');
   });
 });
