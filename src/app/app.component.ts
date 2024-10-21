485090d5c04b7c4e425ac18411a3f24f820075d6
Khumo Mogorosi <khumomogorosi@gmail.com>
2024-10-21 18:24:54 +0200

    feat: add app version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'DevSecOpsApp';
+  @HostBinding('attr.app-version')
+  appVersion = `v${environment.appVersion}`;
 }
