1c92556d9ef8d4969e9dcee8dedb8186466fb76c
Khumo Mogorosi <khumomogorosi@gmail.com>
2024-10-21 18:31:15 +0200

    test: fix broken test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,12 +15,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'DevSecOpsApp' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('DevSecOpsApp');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
